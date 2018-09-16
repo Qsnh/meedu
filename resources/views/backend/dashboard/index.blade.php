c39ec52ab3834d5e6ddd4071bee58c1b3c0d5081
xiaoteng <616896861@qq.com>
2018-09-16 10:53:30 +0800

    fixed:课程详情图片展示bug

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -4,22 +4,10 @@
 
     @include('components.breadcrumb', ['name' => '主面板'])
 
-    <el-row style="text-align: center">
-        <el-col :span="6">
-            <h1>今日会员</h1>
-            <h3>125</h3>
-        </el-col>
-        <el-col :span="6">
-            <h1>今日浏览量</h1>
-            <h3>125</h3>
-        </el-col>
-        <el-col :span="6">
-            <h1>今日充值</h1>
-            <h3>125</h3>
-        </el-col>
-        <el-col :span="6">
-            <h1>今日文章</h1>
-            <h3>125</h3>
+    <el-row>
+        <el-col :span="24" class="backend-dashboard-banner">
+            <h3 style="line-height: 60px">欢迎使用 MeEdu 在线教育点播系统。在这里，您可以开展自己的在线教育。</h3>
+            <p style="color: gray">MeEdu 是一套开源，免费的系统，您可以无限制的使用它而不用担心任何版权问题！</p>
         </el-col>
     </el-row>
 
