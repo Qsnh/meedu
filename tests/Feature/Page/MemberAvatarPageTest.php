b5bffd87b7b5b3a59d8f444e7e80511109a38e8b
Xiaoteng <616896861@qq.com>
2018-09-21 21:38:34 +0800

    Added:用户更换头像页面单元测试

---
+++ tests/Feature/Page/MemberAvatarPageTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Page;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MemberAvatarPageTest extends TestCase
+{
+
+    public function test_member_avatar_page()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user)
+            ->visit(route('member.avatar'))
+            ->see('头像');
+    }
+
+    public function test_avatar_change_action()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user)
+            ->visit(route('member.avatar'))
+            ->attach(base_path('/public/images/meedu.png'), 'file')
+            ->press('更换')
+            ->seePageIs(route('member.avatar'));
+    }
+
+}
