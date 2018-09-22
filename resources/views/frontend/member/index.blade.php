5dd026a568530fc76961ad042f6b9afa6963dfa0
Xiaoteng <616896861@qq.com>
2018-09-22 11:39:45 +0800

    Fixed:会员中心的我的学习展示

--- resources/views/frontend/member/index.blade.php
+++ resources/views/frontend/member/index.blade.php
@@ -11,32 +11,35 @@
                     <p class="text-right">来自：{{$announcement->administrator->name}}</p>
                 @endif
             </div>
-
-            <h3>最近学习</h3>
-            <table class="table table-hover">
-                <thead>
-                <tr>
-                    <td>视频</td>
-                    <td>时间</td>
-                </tr>
-                </thead>
-                <tbody>
-                @forelse($videos as $video)
-                    <tr>
-                        <td>
-                            <a href="{{route('video.show', [$video->course->id, $video->id, $video->slug])}}">
-                                {{$video->title}}
-                            </a>
-                        </td>
-                        <td>{{$video->pivot->created_at}}</td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td class="text-center" colspan="2">暂无记录</td>
-                    </tr>
-                @endforelse
-                </tbody>
-            </table>
         </div>
     @endif
+
+    <div class="row">
+
+        <h3>最近学习</h3>
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <td>视频</td>
+                <td>时间</td>
+            </tr>
+            </thead>
+            <tbody>
+            @forelse($videos as $video)
+                <tr>
+                    <td>
+                        <a href="{{route('video.show', [$video->course->id, $video->id, $video->slug])}}">
+                            {{$video->title}}
+                        </a>
+                    </td>
+                    <td>{{$video->pivot->created_at}}</td>
+                </tr>
+            @empty
+                <tr>
+                    <td class="text-center" colspan="2">暂无记录</td>
+                </tr>
+            @endforelse
+            </tbody>
+        </table>
+    </div>
 @endsection
