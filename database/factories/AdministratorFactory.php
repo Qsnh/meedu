e34c4b7942fe5e0dd85575d3b006789dc8f8f84b
Xiaoteng <616896861@qq.com>
2018-09-24 16:17:38 +0800

    Fixed:模拟数据过长导致单元测试错误

--- database/factories/AdministratorFactory.php
+++ database/factories/AdministratorFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(\App\Models\Administrator::class, function (Faker $faker) {
     return [
         'name' => substr($faker->name, 0, 10),
-        'email' => hexdec(random_bytes(16)).'@163.com',
+        'email' => $faker->randomDigit.'@qq.com',
         'password' => bcrypt('123123'),
     ];
 });
