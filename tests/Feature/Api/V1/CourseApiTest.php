597b5c0b7b22beaf5743dabfa70bbf5feccc26c6
Xiaoteng <616896861@qq.com>
2018-10-12 13:42:39 +0800

    Added:课程评论接口的单元测试

--- tests/Feature/Api/V1/CourseApiTest.php
+++ tests/Feature/Api/V1/CourseApiTest.php
@@ -6,9 +6,11 @@ use App\Http\Resources\CourseResource;
 use App\Http\Resources\VideoRecourse;
 use App\Models\Course;
 use App\Models\Video;
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Passport\Passport;
 use Tests\OriginalTestCase;
 
 class CourseApiTest extends OriginalTestCase
@@ -19,7 +21,7 @@ class CourseApiTest extends OriginalTestCase
         parent::setUp();
 
         config([
-            'meedu.system.cache.status' => 1,
+            'meedu.system.cache.status' => 0,
             'meedu.system.cache.expire' => 100,
         ]);
     }
@@ -30,7 +32,7 @@ class CourseApiTest extends OriginalTestCase
             'is_show' => Course::SHOW_YES,
             'published_at' => Carbon::now()->subDay(1),
         ]);
-        $response = $this->json('get', '/api/v1/courses');
+        $response = $this->json('GET', '/api/v1/courses');
         $response->assertStatus(200);
         foreach ($courses as $index => $item) {
             $response->assertJsonFragment((new CourseResource($item))->toArray(request()));
@@ -43,7 +45,7 @@ class CourseApiTest extends OriginalTestCase
             'published_at' => Carbon::now(),
             'is_show' => Course::SHOW_YES,
         ]);
-        $this->json('get', '/api/v1/course/'.$course->id)
+        $this->json('GET', '/api/v1/course/'.$course->id)
             ->assertStatus(200)
             ->assertJson([
                 'data' => (new CourseResource($course))->toArray(request()),
@@ -56,7 +58,7 @@ class CourseApiTest extends OriginalTestCase
             'published_at' => Carbon::now(),
             'is_show' => Course::SHOW_NO,
         ]);
-        $this->json('get', '/api/v1/course/'.$course->id)
+        $this->json('GET', '/api/v1/course/'.$course->id)
             ->assertStatus(200)
             ->assertExactJson([
                 'message' => 'No query results for model [App\\Models\\Course].',
@@ -89,13 +91,27 @@ class CourseApiTest extends OriginalTestCase
             'published_at' => Carbon::now()->subDay(1),
             'course_id' => $course->id,
         ]);
-        $response = $this->json('get', '/api/v1/course/'.$course->id.'/videos');
+        $response = $this->json('GET', '/api/v1/course/'.$course->id.'/videos');
         $response->assertStatus(200);
         foreach ($videos as $video) {
             $response->assertJsonFragment((new VideoRecourse($video))->toArray(request()));
         }
     }
 
-
+    public function test_course_comment()
+    {
+        $content = '你好，课程123';
+        $course = factory(Course::class)->create([
+            'is_show' => Course::SHOW_YES,
+            'published_at' => Carbon::now()->subDay(1),
+        ]);
+        Passport::actingAs(factory(User::class)->create());
+        $this->json('POST', '/api/v1/course/'.$course->id.'/comment', [
+                'content' => $content,
+            ])
+            ->assertJsonFragment([
+                'content' => markdown_to_html($content),
+            ]);
+    }
 
 }
