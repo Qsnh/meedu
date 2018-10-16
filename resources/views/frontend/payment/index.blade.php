4118495650fab46ce8576399f4db3f67bdf3920c
Xiaoteng <616896861@qq.com>
2018-10-16 15:07:18 +0800

    Fixed:充值界面样式Bug

--- resources/views/frontend/payment/index.blade.php
+++ resources/views/frontend/payment/index.blade.php
@@ -19,11 +19,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-sm-4 recharge-form-box">
-                <div class="row">
-                    <div class="alert alert-warning">
-                        <p><b>注意：</b></p>
-                        <p>1.充值之后无法提现只能在本站消费使用。</p>
-                    </div>
+                <div class="alert alert-warning">
+                    <p><b>注意：</b></p>
+                    <p>1.充值之后无法提现只能在本站消费使用。</p>
                 </div>
                 <form action="" method="post" class="form-horizontal">
                     {!! csrf_field() !!}
@@ -31,11 +29,13 @@
                         <label>金额</label>
                         <div class="input-group">
                             <input type="text" name="money" placeholder="金额" class="form-control">
-                            <div class="input-group-addon">元</div>
+                            <div class="input-group-append">
+                                <span class="input-group-text">元</span>
+                            </div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <button type="submit" class="btn btn-primary">立即充值</button>
+                        <button type="submit" class="btn btn-primary btn-block">立即充值</button>
                     </div>
                 </form>
             </div>
