a816e81414f384f902f7f0e6976689bdbb0602bc
Xiaoteng <616896861@qq.com>
2018-10-22 14:55:54 +0800

    Fixed:有赞云支付回调参数更新导致的支付不成功

--- app/Meedu/Payment/Youzan/Youzan.php
+++ app/Meedu/Payment/Youzan/Youzan.php
@@ -36,7 +36,7 @@ class Youzan implements Payment
             $client = new YZTokenClient($this->getToken());
             $params = [
                 'qr_name' => $payment->getGoodsTitle(),
-                'qr_price' => 1,
+                'qr_price' => $payment->money * 100,
                 'qr_type' => 'QR_TYPE_DYNAMIC',
             ];
             $response = $client->post('youzan.pay.qrcode.create', self::VERSION, $params);
