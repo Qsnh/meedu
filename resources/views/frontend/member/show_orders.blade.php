39d9ff289d5f1dc1dff8073d37895b371b7e604a
Xiaoteng <616896861@qq.com>
2018-10-31 11:53:21 +0800

    Fixed:个人中心我的订单状态显示

--- resources/views/frontend/member/show_orders.blade.php
+++ resources/views/frontend/member/show_orders.blade.php
@@ -12,6 +12,7 @@
             <td>价格</td>
             <td>商品</td>
             <td>时间</td>
+            <td>状态</td>
         </tr>
         </thead>
         <tbody>
@@ -20,10 +21,17 @@
                 <td><span class="badge badge-danger">￥{{ $order->charge }}</span></td>
                 <td>{{ $order->getOrderListTitle() }}</td>
                 <td>{{ $order->created_at }}</td>
+                <td>
+                    @if($order->status == 9)
+                        <span class="badge badge-success">已支付</span>
+                        @else
+                        <span class="badge badge-info">未支付</span>
+                    @endif
+                </td>
             </tr>
         @empty
             <tr>
-                <td class="text-center color-gray" colspan="3">暂无数据</td>
+                <td class="text-center color-gray" colspan="4">暂无数据</td>
             </tr>
         @endforelse
         </tbody>
