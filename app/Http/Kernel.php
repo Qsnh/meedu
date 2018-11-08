48fa71a14c9e62a86868eae2bc2d898cf70a0ed4
Xiaoteng <616896861@qq.com>
2018-11-08 09:09:16 +0800

    跨域支持

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,6 +33,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
 //        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -54,7 +55,6 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
