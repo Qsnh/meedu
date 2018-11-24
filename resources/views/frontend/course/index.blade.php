918a2f0b435e85c387cf510c2f2470e92016462a
Xiaoteng <616896861@qq.com>
2018-11-24 11:55:04 +0800

    课程列表增加价格的显示

--- resources/views/frontend/course/index.blade.php
+++ resources/views/frontend/course/index.blade.php
@@ -23,7 +23,14 @@
                     <div class="card mt-15 mb-15">
                         <img class="card-img-top" style="height: 300px;" data-echo="{{ image_url($course->thumb) }}" src="/images/loading.png">
                         <div class="card-body">
-                            <h5 class="card-title">{{$course->title}}</h5>
+                            <h5 class="card-title">
+                                @if($course->charge > 0)
+                                    <span class="badge badge info">￥{{$course->charge}}</span>
+                                    @else
+                                    <span class="badge badge-success">免费</span>
+                                    @endif
+                                {{$course->title}}
+                            </h5>
                             <p class="card-text">{{$course->short_description}}</p>
                             <a href="{{ route('course.show', [$course->id, $course->slug]) }}" class="btn btn-primary">详情</a>
                         </div>
