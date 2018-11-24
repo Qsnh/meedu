7365fcb23080b74f7b34919cb40f48dd4167f3ae
Xiaoteng <616896861@qq.com>
2018-11-24 10:27:26 +0800

    后台阿里云上传显示ID

--- resources/assets/js/components/VideoUpload.vue
+++ resources/assets/js/components/VideoUpload.vue
@@ -7,8 +7,7 @@
         <el-form-item v-show="show == '使用阿里云视频服务'" label="请选择视频文件（不修改请勿操作）">
             <input type="file" name="file" id="video_files"/>
             <p>上传进度：<span id="upload-progress">暂无</span></p>
-            <input type="hidden" name="aliyun_video_id" id="video_id" v-model="video_id">
-
+            <input type="text" name="aliyun_video_id" id="video_id" v-model="video_id">
             <el-col :span="24">
                 <el-button type="success" id="start-upload">开始上传</el-button>
                 <el-button type="danger" id="stop-upload">停止上传</el-button>
