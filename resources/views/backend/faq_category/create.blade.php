1bd2d91f5f0964ea9e111e52ff331a7fb94beadb
Xiaoteng <13675626825@163.com>
2018-12-01 15:43:26 +0800

    FAQ分类

--- resources/views/backend/faq_category/create.blade.php
+++ resources/views/backend/faq_category/create.blade.php
@@ -4,33 +4,26 @@
 
     @include('components.breadcrumb', ['name' => '添加FAQ分类'])
 
-    <el-row>
-        <el-col :span="24" style="margin-bottom: 20px;">
-            <meedu-a :url="'{{ route('backend.faq.category.index') }}'" :name="'返回列表'"></meedu-a>
-        </el-col>
-    </el-row>
+    <div class="row row-cards">
+        <div class="col-sm-12">
+            <a href="{{ route('backend.faq.category.index') }}" class="btn btn-primary ml-auto">返回列表</a>
+        </div>
+        <div class="col-sm-12">
+            <form action="" method="post">
+                @csrf
+                <div class="form-group">
+                    <label>排序值（整数，升序）</label>
+                    <input type="text" name="sort" class="form-control" placeholder="排序值（整数，升序）" value="{{old('sort')}}">
+                </div>
+                <div class="form-group">
+                    <label>分类名</label>
+                    <input type="text" name="name" class="form-control" placeholder="分类名" value="{{old('name')}}">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">创建</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
-    <el-row :gutter="20">
-        <el-form label-position="top" method="post">
-            {!! csrf_field() !!}
-            <el-col :span="12" :offset="6">
-                <el-form-item label="排序值（整数，升序）">
-                    <el-input name="sort" placeholder="排序值（整数，升序）" value="{{ old('sort') }}"></el-input>
-                </el-form-item>
-
-                <el-form-item label="分类名">
-                    <el-input name="name" placeholder="分类名" value="{{ old('name') }}"></el-input>
-                </el-form-item>
-
-                <el-form-item>
-                    <el-button native-type="submit" type="primary" native-button="submit">添加</el-button>
-                </el-form-item>
-            </el-col>
-        </el-form>
-    </el-row>
-
-@endsection
-
-@section('js')
-    @include('components.vue_init')
 @endsection
--- resources/views/backend/faq_category/edit.blade.php
+++ resources/views/backend/faq_category/edit.blade.php
@@ -4,43 +4,27 @@
 
     @include('components.breadcrumb', ['name' => '编辑FAQ分类'])
 
-    <el-row>
-        <el-col :span="24" style="margin-bottom: 20px;">
-            <meedu-a :url="'{{ route('backend.faq.category.index') }}'" :name="'返回列表'"></meedu-a>
-        </el-col>
-    </el-row>
+    <div class="row row-cards">
+        <div class="col-sm-12">
+            <a href="{{ route('backend.faq.category.index') }}" class="btn btn-primary ml-auto">返回列表</a>
+        </div>
+        <div class="col-sm-12">
+            <form action="" method="post">
+                @csrf
+                <input type="hidden" name="_method" value="PUT">
+                <div class="form-group">
+                    <label>排序值（整数，升序）</label>
+                    <input type="text" name="sort" class="form-control" placeholder="排序值（整数，升序）" value="{{$category->sort}}">
+                </div>
+                <div class="form-group">
+                    <label>分类名</label>
+                    <input type="text" name="name" class="form-control" placeholder="分类名" value="{{$category->name}}">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">保存</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
-    <el-row :gutter="20">
-        <el-form label-position="top" method="post">
-            <input type="hidden" name="_method" value="PUT">
-            {!! csrf_field() !!}
-            <el-col :span="12" :offset="6">
-                <el-form-item label="排序值（整数，升序）">
-                    <el-input name="sort" placeholder="排序值（整数，升序）" v-model="category.sort"></el-input>
-                </el-form-item>
-
-                <el-form-item label="分类名">
-                    <el-input name="name" placeholder="分类名" v-model="category.name"></el-input>
-                </el-form-item>
-
-                <el-form-item>
-                    <el-button native-type="submit" type="primary" native-button="submit">保存</el-button>
-                </el-form-item>
-            </el-col>
-        </el-form>
-    </el-row>
-
-@endsection
-
-@section('js')
-    <script>
-        new Vue({
-            el: '#app',
-            data: function () {
-                return {
-                    category: @json($category)
-                }
-            }
-        });
-    </script>
 @endsection
