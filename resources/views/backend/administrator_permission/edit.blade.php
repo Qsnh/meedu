e1092a6f16f360d5ce09e301650833360d73a39e
Xiaoteng <13675626825@163.com>
2018-12-01 16:51:33 +0800

    权限页面

--- resources/views/backend/administrator_permission/edit.blade.php
+++ resources/views/backend/administrator_permission/edit.blade.php
@@ -4,67 +4,43 @@
 
     @include('components.breadcrumb', ['name' => '编辑权限'])
 
-    <el-row>
-        <el-col :span="24">
-            <meedu-a :url="'{{ route('backend.administrator_permission.index') }}'" :name="'返回权限列表'"></meedu-a>
-        </el-col>
-        <el-col :span="12" :offset="6">
-            <el-form label-position="top" method="post">
+    <div class="row row-cards">
+        <div class="col-sm-12">
+            <a href="{{ route('backend.administrator_permission.index') }}" class="btn btn-primary ml-auto">返回列表</a>
+        </div>
+        <div class="col-sm-12">
+            <form action="" method="post">
                 @csrf
                 <input type="hidden" name="_method" value="PUT">
-                <el-form-item label="Slug">
-                    <el-input name="slug" v-model="permission.slug" disabled></el-input>
-                </el-form-item>
-                <el-form-item label="权限名">
-                    <el-input name="display_name" v-model="permission.display_name" placeholder="请输入权限名"></el-input>
-                </el-form-item>
-                <el-form-item label="描述">
-                    <el-input name="description" v-model="permission.description" placeholder="请输入描述"></el-input>
-                </el-form-item>
-                <el-form-item label="请求方法">
-                    <el-select v-model="selectedMethods" placeholder="请选择" :multiple="true">
-                        <el-option v-for="method in methods"
-                                   :key="method.value"
-                                   :label="method.label"
-                                   :value="method.value"></el-option>
-                    </el-select>
-                    <input type="hidden" name="method[]" :value="method" v-for="method in selectedMethods" :key="method">
-                </el-form-item>
-                <el-form-item label="请求地址[支持正则表达式]">
-                    <el-input name="url" v-model="permission.url" placeholder="请输入请求地址"></el-input>
-                </el-form-item>
-                <el-button type="primary" native-type="submit">编辑</el-button>
-            </el-form>
-        </el-col>
-    </el-row>
+                <div class="form-group">
+                    <label>权限名</label>
+                    <input type="text" name="display_name" value="{{$permission->display_name}}" class="form-control" placeholder="请输入权限名">
+                </div>
+                <div class="form-group">
+                    <label>Slug</label>
+                    <input type="text" name="slug" value="{{$permission->slug}}" class="form-control" placeholder="请输入Slug">
+                </div>
+                <div class="form-group">
+                    <label>描述</label>
+                    <input type="text" name="description" value="{{$permission->description}}" class="form-control" placeholder="请输入描述">
+                </div>
+                <div class="form-group">
+                    <label>请求方式</label>
+                    <select name="method[]" class="form-control" multiple>
+                        <option value="GET" {{in_array('GET', $permission->getMethodArray()) ? 'selected' : ''}}>GET</option>
+                        <option value="POST" {{in_array('POST', $permission->getMethodArray()) ? 'selected' : ''}}>POST</option>
+                        <option value="PUT" {{in_array('PUT', $permission->getMethodArray()) ? 'selected' : ''}}>PUT</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>请求地址[支持正则表达式]</label>
+                    <input type="text" name="url" value="{{$permission->url}}" class="form-control" placeholder="请求地址[支持正则表达式]">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary" type="submit">保存</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
-@endsection
-
-@section('js')
-    <script>
-        var selectedMethods = JSON.parse('@json($permission->getMethodArray())');
-        new Vue({
-            el: '#app',
-            data: function () {
-                return {
-                    methods: [
-                        {
-                            value: 'GET',
-                            label: 'GET'
-                        },
-                        {
-                            value: 'POST',
-                            label: 'POST'
-                        },
-                        {
-                            value: 'PUT',
-                            label: 'PUT'
-                        }
-                    ],
-                    selectedMethods: selectedMethods,
-                    permission: @json($permission)
-                }
-            }
-        });
-    </script>
 @endsection
