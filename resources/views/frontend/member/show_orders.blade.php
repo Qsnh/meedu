d92ff2d53c99d82ebdfd7c70ed63248053eef1aa
Xiaoteng <616896861@qq.com>
2018-12-08 12:31:03 +0800

    Added:未支付订单继续支付

--- resources/views/frontend/member/show_orders.blade.php
+++ resources/views/frontend/member/show_orders.blade.php
@@ -13,6 +13,7 @@
             <td>商品</td>
             <td>时间</td>
             <td>状态</td>
+            <td>操作</td>
         </tr>
         </thead>
         <tbody>
@@ -25,13 +26,18 @@
                     @if($order->status == 9)
                         <span class="badge badge-success">已支付</span>
                         @else
-                        <span class="badge badge-info">未支付</span>
+                        <span class="badge badge-primary">未支付</span>
+                    @endif
+                </td>
+                <td>
+                    @if($order->status != 9)
+                        <a href="{{route('order.show', $order->order_id)}}" class="btn btn-primary btn-sm">继续支付</a>
                     @endif
                 </td>
             </tr>
         @empty
             <tr>
-                <td class="text-center color-gray" colspan="4">暂无数据</td>
+                <td class="text-center color-gray" colspan="5">暂无数据</td>
             </tr>
         @endforelse
         </tbody>
