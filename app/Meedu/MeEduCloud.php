69c0431ed3bb70353ef6afd89224dd4aad49fdae
Xiaoteng <616896861@qq.com>
2019-01-13 11:37:09 +0800

    resolve #21

--- app/Meedu/MeEduCloud.php
+++ app/Meedu/MeEduCloud.php
@@ -1,13 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: xiaoteng
- * Date: 2019/1/4
- * Time: 08:44
+
+/*
+ * This file is part of the Qsnh/meedu.
+ *
+ * (c) XiaoTeng <616896861@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace App\Meedu;
-
 
 use GuzzleHttp\Client;
 
@@ -42,5 +44,4 @@ class MeEduCloud
             ],
         ]);
     }
-
 }
