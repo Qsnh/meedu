9ad802d8f29c43f3c7e6adc37fcf8480807a6a8c
Xiaoteng <616896861@qq.com>
2019-01-13 12:03:45 +0800

    Added:网站名彩蛋

--- resources/views/components/frontend/header.blade.php
+++ resources/views/components/frontend/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'MeEdu') }}
+            {!! config('app.name', 'MeEdu') !!}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
