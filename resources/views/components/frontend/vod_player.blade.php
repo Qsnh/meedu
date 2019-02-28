1b06b83b01858c59c6c344453cbe4cc58146c66d
Xiaoteng <616896861@qq.com>
2019-02-28 22:23:09 +0800

    腾讯云播放器

---
+++ resources/views/components/frontend/vod_player.blade.php
@@ -0,0 +1,10 @@
+<link href="//imgcache.qq.com/open/qcloud/video/tcplayer/tcplayer.css" rel="stylesheet">
+<script src="//imgcache.qq.com/open/qcloud/video/tcplayer/lib/hls.min.0.8.8.js"></script>
+<script src="//imgcache.qq.com/open/qcloud/video/tcplayer/tcplayer.min.js"></script>
+<video id="xiaoteng-vod-player" width="825" height="500" preload="auto" playsinline webkit-playsinline></video>
+<script>
+    var vodPlayer = TCPlayer('xiaoteng-vod-player', {
+        fileID: '4564972818956091133',
+        appID: '1253668508'
+    });
+</script>
