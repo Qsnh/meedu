c1bfb4cc8bec901be41e44ab6f55a5f0960a59d3
Xiaoteng <616896861@qq.com>
2019-03-31 20:12:15 +0800

    完成首页的替换

--- resources/views/frontend/index/index.blade.php
+++ resources/views/frontend/index/index.blade.php
@@ -2,86 +2,170 @@
 
 @section('content')
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm index-banner text-center">
-                <h1>书山有路勤为径，学海无涯苦作舟</h1>
-                <h4>你想要的，这里都有！</h4>
-                <p class="button">
-                    <a href="{{ route('courses') }}">立即订阅</a>
-                </p>
+    <div class="container-fluid bg-primary pt-60 pb-40">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <div class="card p-30 pt-50 text-center">
+                        <div>
+                            <a class="avatar avatar-xxl status-success mb-3" href="#">
+                                <img src="../assets/img/avatar/1.jpg">
+                            </a>
+                        </div>
+                        <h5>小滕</h5>
+                        <p><small class="fs-13">PHP全栈开发工程师</small></p>
+                        <p class="text-light fs-12 mb-50 fw-500">
+                            会一点PHP，会一点GO。知名在线点播系统MeEdu作者，已录制10余套课程。
+                        </p>
+                        <div class="gap-items fs-16">
+                            <a class="text-facebook" target="_blank" href="https://github.com/Qsnh"><i class="fa fa-github"></i></a>
+                            <a class="text-dribbble" target="_blank" href="#"><i class="fa fa-qq"></i></a>
+                            <a class="text-twitter" target="_blank" href="#"><i class="fa fa-wechat"></i></a>
+                            <a class="text-google" target="_blank" href="https://weibo.com/xiaotenggogo"><i class="fa fa-weibo"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-8 col-sm-12">
+                    <div class="card">
+                        <h4 class="card-title"><strong>关于学习的看法</strong></h4>
+                        <div class="card-body text-dark fw-500">
+                            <p>
+                                很多人对花几百块钱吃一顿饭毫不犹豫，但是对花几百块钱投入学习却犹豫不决。你说TA舍得消费，但是TA又不舍得消费。其实，这很正常，花钱吃饭是一件享受的事情，
+                                但是花钱学习却是一件痛苦的事情，学习从来都不是快乐的事情，这点毋庸置疑。所以，很多情况下，人们总是待在自己的舒适区内，并不是TA不愿意投入，而是不愿意逃离
+                                自己的舒适圈。
+                            </p>
+                            <p>
+                                2019年很多互联网公司都在裁员，裁员规模颇大，很多人失业。其实互联网发展到现在，早期的红利到现在已经分的差不多了，如果技术能力不过关，很有可能就在这次大浪之中被淘汰。
+                                这并不是危言耸听，你会发现，现在很多大学生的技术能力都超过一线的工作人员，这个现象想在越来越普遍，究其原因主要是高校的专业化培养和越来越多的学习资料。一线中的技术人员
+                                如果没有持续学习的能力，很快就会被技术能力强的年轻人所取代。
+                            </p>
+                            <p>
+                                做技术必须要保持终生学习的习惯！
+                            </p>
+                            <p class="text-right">
+                                小滕 于2019年3月29日
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
 
-    <div class="container">
-        <div class="row index-banner2">
-
+    <div class="container pt-40 pb-40">
+        <div class="row">
             <div class="col-sm-12">
-                <h2 class="text-center index-banner-title">最新课程</h2>
+                <div class="divider"><a class="fs-24" href="#">最新课程</a></div>
             </div>
+            <div class="col-sm-12">
+                <div class="card-deck">
+                    @foreach($courses as $course)
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <img class="card-img-top" src="{{ image_url($course->thumb) }}" alt="{{$course->title}}">
+                                <div class="card-body">
+                                    <h4 class="card-title b-0 px-0">
+                                        <a href="{{ route('course.show', [$course->id, $course->slug]) }}">{{$course->title}}</a>
+                                    </h4>
+                                    <p><small>最后更新：{{$course->updated_at->diffForHumans()}}</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 
-            @foreach($courses as $course)
-                <div class="col-sm-4">
-                    <div class="card mt-15 mb-15">
-                        <img class="card-img-top" style="height: 300px;" data-echo="{{ image_url($course->thumb) }}" src="/images/loading.png">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$course->title}}</h5>
-                            <p class="card-text">{{$course->short_description}}</p>
-                            <a href="{{ route('course.show', [$course->id, $course->slug]) }}" class="btn btn-primary">详情</a>
+    <div class="container-fluid pt-40 pb-40 bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="divider"><span class="fs-24">评价</span></div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="card card-inverse" style="background-color: #3b5998">
+                        <div class="card-header no-border">
+                            <h5 class="card-title card-title-bold">小滕</h5>
+                        </div>
+                        <blockquote class="blockquote blockquote-inverse no-border card-body m-0">
+                            <p>MeEdu 非常棒，我在几分钟之内就搭建好了自己的知识付费应用，感谢 MeEdu </p>
+                            <div class="flexbox">
+                                <time class="text-white" datetime="2017-10-02 20:00">2019/3/31</time>
+                            </div>
+                        </blockquote>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-4">
+                    <div class="card card-inverse" style="background-color: #3b5998">
+                        <div class="card-header no-border">
+                            <h5 class="card-title card-title-bold">小滕</h5>
+                        </div>
+                        <blockquote class="blockquote blockquote-inverse no-border card-body m-0">
+                            <p>MeEdu 非常棒，我在几分钟之内就搭建好了自己的知识付费应用，感谢 MeEdu </p>
+                            <div class="flexbox">
+                                <time class="text-white" datetime="2017-10-02 20:00">2019/3/31</time>
+                            </div>
+                        </blockquote>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-4">
+                    <div class="card card-inverse" style="background-color: #3b5998">
+                        <div class="card-header no-border">
+                            <h5 class="card-title card-title-bold">小滕</h5>
+                        </div>
+                        <blockquote class="blockquote blockquote-inverse no-border card-body m-0">
+                            <p>MeEdu 非常棒，我在几分钟之内就搭建好了自己的知识付费应用，感谢 MeEdu </p>
+                            <div class="flexbox">
+                                <time class="text-white" datetime="2017-10-02 20:00">2019/3/31</time>
+                            </div>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container pt-70 pb-40">
+        <div class="row">
+            @foreach($roles as $index => $role)
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <h5 class="text-uppercase text-muted">{{$role->name}}</h5>
+                            <br>
+                            <h3 class="price">
+                                <sup>￥</sup>{{$role->charge}}
+                                <span>&nbsp;</span>
+                            </h3>
+                            <hr>
+                            @foreach($role->descriptionRows() as $row)
+                                <p>{{$row}}</p>
+                            @endforeach
+                            <br><br>
+                            <a class="btn btn-bold btn-block btn-primary" href="{{route('role.index')}}">立即订阅</a>
                         </div>
                     </div>
                 </div>
             @endforeach
-
-        </div>
-    </div>
-
-    <div class="container-fluid roles">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-sm-12">
-                            <h2 class="text-center index-banner-title">计划</h2>
-                        </div>
-
-                        @foreach($roles as $role)
-                            <div class="col-sm-4 roles-item">
-                                <div class="roles-item-info text-center border">
-                                    <h2 class="title">{{$role->name}}</h2>
-                                    @foreach($role->descriptionRows() as $row)
-                                        <p>{{$row}}</p>
-                                    @endforeach
-                                    <p class="price">￥{{$role->charge}}</p>
-                                    <p>
-                                        <a href="{{route('role.index')}}" class="buy-button">立即订阅</a>
-                                    </p>
-                                </div>
-                            </div>
-                        @endforeach
-
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 
     @if(!$links->isEmpty())
-    <div class="container-fluid index-links">
-        <div class="row">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <h5>友情链接</h5>
-                        @foreach($links as $link)
-                            <a href="{{$link->url}}" target="_blank">{{$link->name}}</a>
-                        @endforeach
-                    </div>
+        <div class="container pt-40 pb-40">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h5>友情链接</h5>
+                    @foreach($links as $link)
+                        <a href="{{$link->url}}" target="_blank">{{$link->name}}</a>
+                    @endforeach
                 </div>
             </div>
         </div>
-    </div>
     @endif
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<!doctype html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,39 +8,145 @@
     <meta name="keywords" content="{{$keywords ?? ''}}">
     <meta name="description" content="{{$description ?? ''}}">
     <title>{{Auth::check() ? Auth::user()->nick_name.' - ' : ''}}{{$title ?? 'MeEdu'}}</title>
-    <link href="https://lib.baomitu.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="{{ mix('css/frontend.css') }}" rel="stylesheet">
-    <script src="{{ mix('js/frontend.js') }}"></script>
-    @yield('css')
+    <!-- Styles -->
+    <link href="{{asset('/frontend/assets/css/core.min.css')}}" rel="stylesheet">
+    <link href="{{asset('/frontend/assets/css/app.min.css')}}" rel="stylesheet">
+    <link href="{{asset('/frontend/assets/css/style.min.css')}}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        @include('components.frontend.header')
-        @yield('content')
+<body style="padding-top: 64px;">
+
+<header class="topbar topbar-unfix">
+    <div class="topbar-left">
+        <span class="topbar-brand"><img src="{{asset('/frontend/logo.png')}}" alt="logo-icon"></span>
+        <div class="topbar-divider d-none d-xl-block"></div>
+        <nav class="topbar-navigation">
+            <ul class="menu">
+                <li class="menu-item active">
+                    <a class="menu-link" href="{{url('/')}}">
+                        <span class="icon ti-home"></span>
+                        <span class="title">首页</span>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a class="menu-link" href="{{route('courses')}}">
+                        <span class="icon ti-headphone-alt"></span>
+                        <span class="title">课程</span>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a class="menu-link" href="{{route('role.index')}}">
+                        <span class="icon ti-gift"></span>
+                        <span class="title">订阅</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="topbar-right">
+        <ul class="topbar-btns">
+            <li class="dropdown">
+                <span class="topbar-btn" data-toggle="dropdown"><img class="avatar" src="../assets/img/avatar/1.jpg" alt="..."></span>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a class="dropdown-item" href="#"><i class="ti-user"></i> Profile</a>
+                    <a class="dropdown-item" href="#"><i class="ti-settings"></i> Settings</a>
+                    <a class="dropdown-item" href="#"><i class="ti-help"></i> Help</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#"><i class="ti-power-off"></i> Logout</a>
+                </div>
+            </li>
+
+        </ul>
+
+    </div>
+</header>
+
+@yield('content')
+
+<footer class="site-footer pt-50 pb-20">
+    <div class="row gap-y">
+        <div class="col-lg-5">
+            <h5 class="text-uppercase fs-14 ls-1">关于我们</h5>
+            <p class="text-justify">
+                MeEdu 是一款转为个人开发的开源免费的在线点播系统。依靠 MeEdu 可以在短短几分钟之内搭建一个功能完善的在线教育系统。MeEdu 专注与视频
+                的付费点播，结合数百款插件足以满足您的任何要求。
+            </p>
+        </div>
+
+        <div class="col-6 col-md-4 col-lg-2 text-left1 text-lg-center1">
+            <h5 class="text-uppercase fs-14 ls-1">MeEdu</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://github.com/Qsnh/meedu">源码</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://github.com/Qsnh/meedu/issues">Bug反馈</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-6 col-md-4 col-lg-2 text-left1 text-lg-center1">
+            <h5 class="text-uppercase fs-14 ls-1">服务支持</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://meedu.vip">官网</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://meedu.vip">插件</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-6 col-md-4 col-lg-2 text-left1 text-lg-center1">
+            <h5 class="text-uppercase fs-14 ls-1">服务支持</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://meedu.vip">官网</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://meedu.vip">插件</a>
+                </li>
+            </ul>
+        </div>
+
     </div>
 
-    @include(config('meedu.advance.layout_footer'))
-    <script src="{{ asset('js/echo.min.js') }}"></script>
-    <script crossorigin="anonymous" integrity="sha384-RIQuldGV8mnjGdob13cay/K1AJa+LR7VKHqSXrrB5DPGryn4pMUXRLh92Ev8KlGF" src="https://lib.baomitu.com/sweetalert/2.1.2/sweetalert.min.js"></script>
-    <script>
-        echo.init({
-            offset: 100,
-            throttle: 250,
-            unload: false
-        });
+    <hr>
 
-        // 消息提示
-        @if(get_first_flash('success'))
-        swal("成功", "{{get_first_flash('success')}}", "success");
-        @endif
-        @if(get_first_flash('warning'))
-        swal("警告", "{{get_first_flash('warning')}}", "warning");
-        @endif
-        @if(get_first_flash('error'))
-        swal("错误", "{{get_first_flash('error')}}", "error");
-        @endif
-    </script>
-    @yield('js')
-    <div style="display:none">{!! config('meedu.system.js') !!}</div>
+    <div class="row">
+        <div class="col-md-6 justify-content-center justify-content-md-start">
+            <p>Copyright © 2019 <a href="https://github.com/Qsnh/meedu">MeEdu</a>. All rights reserved.</p>
+        </div>
+
+        <div class="col-md-6">
+            <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://github.com/Qsnh/meedu">源码</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://meedu.vip">Website</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
+
+
+<!-- Scripts -->
+<script src="{{asset('/frontend/assets/js/core.min.js')}}" data-provide="sweetalert2"></script>
+<script src="{{asset('/frontend/assets/js/app.min.js')}}"></script>
+<script src="{{asset('/frontend/assets/js/script.min.js')}}"></script>
+<script>
+    @if(get_first_flash('success'))
+    swal("成功", "{{get_first_flash('success')}}", "success");
+    @endif
+    @if(get_first_flash('warning'))
+    swal("警告", "{{get_first_flash('warning')}}", "warning");
+    @endif
+    @if(get_first_flash('error'))
+    swal("错误", "{{get_first_flash('error')}}", "error");
+    @endif
+</script>
+@yield('js')
+<div style="display:none">{!! config('meedu.system.js') !!}</div>
 </body>
 </html>
