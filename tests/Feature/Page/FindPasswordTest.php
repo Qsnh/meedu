29c29adf9ce66619369ecbf736f4453e2005b6e4
小滕 <xiaoteng@192.168.0.103>
2019-07-27 15:16:56 +0800

    add:首页测试

--- tests/Feature/Page/FindPasswordTest.php
+++ tests/Feature/Page/FindPasswordTest.php
@@ -12,7 +12,10 @@ class FindPasswordTest extends TestCase
     // 测试找回密码
     public function test_visit()
     {
-        $this->get(route('password.request'))->assertResponseStatus(200);
+        $this->get(route('password.request'))
+            ->assertResponseStatus(200)
+            ->see('手机号')
+            ->seeInElement('button', '重置密码');
     }
 
 }
