e313f76ecf709c78130c80893a682ff6f957ea0b
小滕 <xiaoteng@192.168.0.103>
2019-07-27 14:06:24 +0800

    fixed:课程详情和课程列表页面测试

--- tests/Feature/Page/CourseListPageTest.php
+++ tests/Feature/Page/CourseListPageTest.php
@@ -11,11 +11,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class CourseListPageTest extends TestCase
 {
 
+    // 可以正常的访问课程列表页面
     public function test_visit_course_list_page()
     {
         $this->get(route('courses'))->assertResponseStatus(200);
     }
 
+    // 显示且已发布的课程可以在课程列表页面看到
     public function test_visit_course_list_show()
     {
         $course = factory(Course::class)->create([
@@ -27,6 +29,7 @@ class CourseListPageTest extends TestCase
         $response->see($course->title);
     }
 
+    // 不显示的课程无法再课程列表页面看到
     public function test_visit_course_list_hide()
     {
         $course = factory(Course::class)->create([
@@ -38,17 +41,7 @@ class CourseListPageTest extends TestCase
         $response->dontSee($course->title);
     }
 
-    public function test_visit_course_list_published()
-    {
-        $course = factory(Course::class)->create([
-            'is_show' => Course::SHOW_YES,
-            'published_at' => Carbon::now()->subDay(1),
-        ]);
-        $response = $this->get(route('courses'));
-        $response->assertResponseStatus(200);
-        $response->see($course->title);
-    }
-
+    // 未发布的课程无法再课程列表界面看到
     public function test_visit_course_list_no_published()
     {
         $course = factory(Course::class)->create([
@@ -60,6 +53,7 @@ class CourseListPageTest extends TestCase
         $response->dontSee($course->title);
     }
 
+    // 可以看到分页组件
     public function test_visit_course_see_pagination()
     {
         factory(Course::class, 100)->create();
