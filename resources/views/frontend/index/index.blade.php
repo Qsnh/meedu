a144bc6bbbd308eb57268db1003ef2d7645d5574
小滕 <xiaoteng@192.168.0.103>
2019-09-24 20:09:43 +0800

    更换首页banner

--- resources/views/frontend/index/index.blade.php
+++ resources/views/frontend/index/index.blade.php
@@ -5,51 +5,19 @@
     <div class="container-fluid bg-primary pt-60 pb-40">
         <div class="container">
             <div class="row">
-                <div class="col-md-4 col-sm-12">
-                    <div class="card p-30 pt-50 text-center hover-shadow-2">
-                        <div>
-                            <a class="avatar avatar-xxl status-success mb-3" href="#">
-                                <img src="/frontend/assets/img/avatar/1.jpg">
-                            </a>
-                        </div>
-                        <h5>小滕</h5>
-                        <p><small class="fs-13">PHP全栈开发工程师</small></p>
-                        <p class="text-light fs-12 mb-50 fw-500">
-                            会一点PHP，会一点GO。知名在线点播系统MeEdu作者，已录制10余套课程。
-                        </p>
-                        <div class="gap-items fs-16">
-                            <a class="text-facebook" target="_blank" href="https://github.com/Qsnh"><i class="fa fa-github"></i></a>
-                            <a class="text-dribbble" target="_blank" href="#"><i class="fa fa-qq"></i></a>
-                            <a class="text-twitter" target="_blank" href="#"><i class="fa fa-wechat"></i></a>
-                            <a class="text-google" target="_blank" href="https://weibo.com/xiaotenggogo"><i class="fa fa-weibo"></i></a>
-                        </div>
-                    </div>
+                <div class="col-md-4 col-sm-12" style="padding-top: 160px;">
+                    <h1 style="padding: 10px 20px; background-color: #fff; color: #4ed2c5; font-weight: 800;">一切，从这里开始。</h1>
+                    <p class="mt-4">
+                        MeEdu 是基于 Laravel 开发的在线基于系统。使用它，你可以在几分钟之内搭建好完善的在线教育系统，它让你更加聚焦教学的开发！技术方面，它已经帮你做好啦！
+                    </p>
+                    <p class="mt-4">
+                        <a href="{{route('courses')}}" class="btn btn-lg btn-danger">全部课程</a>
+                    </p>
                 </div>
 
                 <div class="col-md-8 col-sm-12">
-                    <div class="card hover-shadow-2">
-                        <h4 class="card-title"><strong>关于学习的看法</strong></h4>
-                        <div class="card-body text-dark fw-500">
-                            <p>
-                                很多人对花几百块钱吃一顿饭毫不犹豫，但是对花几百块钱投入学习却犹豫不决。你说TA舍得消费，但是TA又不舍得消费。其实，这很正常，花钱吃饭是一件享受的事情，
-                                但是花钱学习却是一件痛苦的事情，学习从来都不是快乐的事情，这点毋庸置疑。所以，很多情况下，人们总是待在自己的舒适区内，并不是TA不愿意投入，而是不愿意逃离
-                                自己的舒适圈。
-                            </p>
-                            <p>
-                                2019年很多互联网公司都在裁员，裁员规模颇大，很多人失业。其实互联网发展到现在，早期的红利到现在已经分的差不多了，如果技术能力不过关，很有可能就在这次大浪之中被淘汰。
-                                这并不是危言耸听，你会发现，现在很多大学生的技术能力都超过一线的工作人员，这个现象想在越来越普遍，究其原因主要是高校的专业化培养和越来越多的学习资料。一线中的技术人员
-                                如果没有持续学习的能力，很快就会被技术能力强的年轻人所取代。
-                            </p>
-                            <p>
-                                做技术必须要保持终生学习的习惯！
-                            </p>
-                            <p class="text-right">
-                                小滕 于2019年3月29日
-                            </p>
-                        </div>
-                    </div>
+                    <img src="/images/index-banner-img.svg">
                 </div>
-
             </div>
         </div>
     </div>
