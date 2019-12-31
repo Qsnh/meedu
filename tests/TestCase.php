942db79e7d588dce8479a696d63272a42f4c20f8
小滕 <xiaoteng@192.168.0.103>
2019-12-31 13:59:05 +0800

    加快单元测试速度

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,12 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, DatabaseMigrations;
+    use CreatesApplication, RefreshDatabase;
 
     public $baseUrl = 'http://127.0.0.1:8000';
 
