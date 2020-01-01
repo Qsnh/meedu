c27deb4ceafc79fb0efae3d0ef9ac03673f362be
小滕 <xiaoteng@192.168.0.103>
2020-01-01 19:58:01 +0800

    fixed: 手动支付界面的样式

--- resources/views/frontend/order/hand_pay.blade.php
+++ resources/views/frontend/order/hand_pay.blade.php
@@ -12,7 +12,7 @@
 
     <div class="container all-buy-box">
         <div class="row justify-content-center">
-            <div class="col-sm-4 text-center" style="min-height: 400px;">
+            <div class="col-sm-12 text-center" style="min-height: 400px;">
                 <p>订单号：{{$order['order_id']}}</p>
                 <p>支付金额：￥{{$order['charge']}}</p>
                 <p class="mt-5 mb-5">{!! $intro !!}</p>
