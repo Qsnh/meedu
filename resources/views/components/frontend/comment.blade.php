00aa170eb11697184a4a6021a9b886ca6cd2766d
小滕 <xiaoteng@192.168.0.103>
2020-01-02 09:05:15 +0800

    fixed: 评论报错的消息展示

--- resources/views/components/frontend/comment.blade.php
+++ resources/views/components/frontend/comment.blade.php
@@ -3,38 +3,43 @@
         <div class="col-sm-12">
             <div class="publisher publisher-multi bg-white b-1 mb-30">
                 @auth
-                    <textarea class="publisher-input auto-expand" name="comment_content" id="comment-content" rows="2" placeholder="写点吧"></textarea>
-                    <p class="text-right"><button type="button" id="submit-comment" class="btn btn-sm btn-bold btn-primary">评论</button></p>
-                    @else
-                    <textarea class="publisher-input auto-expand" name="comment_content" id="comment-content" rows="1" placeholder="请先登录" disabled="disabled"></textarea>
+                    <textarea class="publisher-input auto-expand" name="comment_content" id="comment-content" rows="2"
+                              placeholder="写点吧"></textarea>
+                    <p class="text-right">
+                        <button type="button" id="submit-comment" class="btn btn-sm btn-bold btn-primary">评论</button>
+                    </p>
+                @else
+                    <textarea class="publisher-input auto-expand" name="comment_content" id="comment-content" rows="1"
+                              placeholder="请先登录" disabled="disabled"></textarea>
                 @endauth
             </div>
         </div>
         <div class="col-sm-12 comment-box">
             @foreach($comments as $comment)
-            <div class="card">
-                <div class="card-body">
-                    <div class="media bb-1 border-fade">
-                        <img class="avatar avatar-lg" src="{{$users[$comment['user_id']]['avatar'] ?? ''}}">
-                        <div class="media-body">
-                            <p>
-                                <strong class="fs-14">{{$users[$comment['user_id']]['nick_name']}}</strong>
-                                <time class="float-right text-lighter" datetime="{{$comment['created_at']}}">{{$comment['created_at']}}</time>
-                            </p>
-                            <p>
-                                @if($users[$comment['user_id']]['role'] ?? '')
-                                    <span class="badge badge-primary">{{$users[$comment['user_id']]['role']['name'] ?? ''}}</span>
-                                @else
-                                    <span class="badge badge-default">免费会员</span>
-                                @endif
-                            </p>
+                <div class="card">
+                    <div class="card-body">
+                        <div class="media bb-1 border-fade">
+                            <img class="avatar avatar-lg" src="{{$users[$comment['user_id']]['avatar'] ?? ''}}">
+                            <div class="media-body">
+                                <p>
+                                    <strong class="fs-14">{{$users[$comment['user_id']]['nick_name']}}</strong>
+                                    <time class="float-right text-lighter"
+                                          datetime="{{$comment['created_at']}}">{{$comment['created_at']}}</time>
+                                </p>
+                                <p>
+                                    @if($users[$comment['user_id']]['role'] ?? '')
+                                        <span class="badge badge-primary">{{$users[$comment['user_id']]['role']['name'] ?? ''}}</span>
+                                    @else
+                                        <span class="badge badge-default">免费会员</span>
+                                    @endif
+                                </p>
+                            </div>
+                        </div>
+                        <div class="card-body border-fade">
+                            {!! $comment['render_content'] !!}
                         </div>
                     </div>
-                    <div class="card-body border-fade">
-                        {!! $comment['render_content'] !!}
-                    </div>
                 </div>
-            </div>
             @endforeach
         </div>
     </div>
@@ -44,12 +49,20 @@
     <script>
         var submitBtn = document.getElementById('submit-comment');
         submitBtn.addEventListener('click', function () {
-           var content = document.getElementById('comment-content').value;
-           if (content === '') {
+            var content = document.getElementById('comment-content').value;
+            if (content === '') {
                 swal('Oops', '请输入内容', 'error');
                 return;
-           }
+            }
+            if (content.length < 6) {
+                swal('Oops', '评论内容不能少于6个字', 'error');
+                return;
+            }
             $(this).attr('disabled', true);
+            $(document).ajaxError(function (event, xhr) {
+                $('#submit-comment').removeAttr('disabled');
+                swal("失败", xhr.responseJSON.errors.content[0], "error");
+            });
             $.post("{{$submitUrl}}", {
                 'content': $('textarea[name="comment_content"]').val(),
                 '_token': '{{csrf_token()}}'
@@ -79,9 +92,9 @@ ${res.content}
                         </div>
                     </div>
                 </div>
-`);
+`)
                 }
             }, 'json');
         });
     </script>
-    @endsection
+@endsection
