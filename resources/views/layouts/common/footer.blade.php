8bff5b173b509cfe5bbffa331930a5d795a28051
小滕 <xiaoteng@192.168.0.103>
2020-03-09 18:48:53 +0800

    界面细节优化

--- resources/views/layouts/common/footer.blade.php
+++ resources/views/layouts/common/footer.blade.php
@@ -59,7 +59,7 @@
                 <div class="input-group">
                     <input type="text" name="captcha" placeholder="验证码" class="form-control" required>
                     <div class="input-group-append">
-                        <img src="{{ captcha_src() }}" class="captcha" width="120" height="36">
+                        <img src="{{ captcha_src() }}" class="captcha" width="120" height="48">
                     </div>
                 </div>
             </div>
@@ -106,7 +106,7 @@
             <div class="input-group">
                 <input type="text" name="captcha" placeholder="验证码" class="form-control" required>
                 <div class="input-group-append">
-                    <img src="{{ captcha_src() }}" class="captcha" width="120" height="36">
+                    <img src="{{ captcha_src() }}" class="captcha" width="120" height="48">
                 </div>
             </div>
         </div>
@@ -154,7 +154,7 @@
             <div class="input-group">
                 <input type="text" name="captcha" placeholder="验证码" class="form-control" required>
                 <div class="input-group-append">
-                    <img src="{{ captcha_src() }}" class="captcha" width="120" height="36">
+                    <img src="{{ captcha_src() }}" class="captcha" width="120" height="48">
                 </div>
             </div>
         </div>
@@ -205,7 +205,7 @@
             <div class="input-group">
                 <input type="text" name="captcha" placeholder="验证码" class="form-control" required>
                 <div class="input-group-append">
-                    <img src="{{ captcha_src() }}" class="captcha" width="120" height="36">
+                    <img src="{{ captcha_src() }}" class="captcha" width="120" height="48">
                 </div>
             </div>
         </div>
