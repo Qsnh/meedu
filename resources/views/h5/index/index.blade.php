e8aed1853757f070b7c4c5cd11f5ae2ddd9d5171
小滕 <xiaoteng@192.168.0.103>
2020-03-23 11:10:05 +0800

    fixed: h5课程布局宽度不一致

--- resources/views/h5/index/index.blade.php
+++ resources/views/h5/index/index.blade.php
@@ -30,15 +30,7 @@
             <div class="banner-title">{{$banner['name']}}</div>
             <div class="courses">
                 @foreach($banner['courses'] as $index => $course)
-                    <a href="{{route('course.show', [$course['id'], $course['slug']])}}"
-                       class="banner-course-item {{$index % 2 === 0 ? 'first' : ''}}">
-                        <div class="course-thumb" style="background-image: url('{{$course['thumb']}}')"></div>
-                        <div class="course-title">{{$course['title']}}</div>
-                        <div class="course-info">
-                            <span class="course-category">{{$course['category']['name']}}</span>
-                            <span class="course-video-count">{{$course['videos_count']}}课时</span>
-                        </div>
-                    </a>
+                    @include('h5.components.course', ['course' => $course])
                 @endforeach
             </div>
         </div>
--- resources/views/h5/member/buy_course.blade.php
+++ resources/views/h5/member/buy_course.blade.php
@@ -23,15 +23,7 @@
                     @if(!($course = $courses[$record['course_id']] ?? []))
                         @continue
                     @endif
-                    <a href="{{route('course.show', [$course['id'], $course['slug']])}}"
-                       class="banner-course-item {{$index % 2 === 0 ? 'first' : ''}}">
-                        <div class="course-thumb" style="background-image: url('{{$course['thumb']}}')"></div>
-                        <div class="course-title">{{$course['title']}}</div>
-                        <div class="course-info">
-                            <span class="course-category">{{$course['category']['name']}}</span>
-                            <span class="course-video-count">{{$course['videos_count']}}课时</span>
-                        </div>
-                    </a>
+                    @include('h5.components.course', ['course' => $course])
                 @empty
                     @include('h5.components.none')
                 @endforelse
