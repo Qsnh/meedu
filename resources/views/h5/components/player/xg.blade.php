570e5fd0fc94937e0143e318fcfdd22db14fc24b
小滕 <xiaoteng@192.168.0.103>
2020-04-24 10:38:52 +0800

    优化微信内置视频播放

--- resources/views/h5/components/player/xg.blade.php
+++ resources/views/h5/components/player/xg.blade.php
@@ -5,14 +5,15 @@
         width: window.innerWidth,
         height: 192,
         poster: "{{$gConfig['system']['player_thumb']}}",
-        playbackRate: [0.5, 0.75, 1, 1.5, 2],
+        // playbackRate: [0.5, 0.75, 1, 1.5, 2],
         defaultPlaybackRate: 1,
         url: "{!! $playUrls->first()['url'] !!}",
-        'x5-video-player-type': 'h5',
-        'x5-video-player-fullscreen': false,
-        'x5-video-orientation': 'landscape',
-        playsinline: true,
-        airplay: true
+        // 'x5-video-player-type': 'h5',
+        // 'x5-video-player-fullscreen': false,
+        // 'x5-video-orientation': 'landscape',
+        playsinline: false,
+        airplay: true,
+        useHls: true
     };
             @if($playUrls->first()['format'] === 'm3u8')
     const XGPlayer = new HlsJsPlayer(XGPlayerConfig);
