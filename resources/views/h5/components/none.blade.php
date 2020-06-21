9d3302fcb78f8327cb68596e69be0c8325544005
小滕 <xiaoteng@192.168.0.103>
2020-06-21 15:19:49 +0800

    h5暂无组件大小调整

--- resources/views/h5/components/none.blade.php
+++ resources/views/h5/components/none.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <div class="col-12 text-center my-5">
-            <img src="/images/icons/none.png" width="60" height="60">
+            <img src="/images/icons/none.png" width="100" height="100">
         </div>
     </div>
 </div>
