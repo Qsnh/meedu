ad73c005749c2be6977085a4595bd1a8295e7040
xtt <xx@xx.com>
2021-07-26 14:07:19 +0800

    后台首页dashboard api统计数据

--- app/Http/Controllers/Backend/Api/V1/DashboardController.php
+++ app/Http/Controllers/Backend/Api/V1/DashboardController.php
@@ -8,6 +8,7 @@
 
 namespace App\Http\Controllers\Backend\Api\V1;
 
+use Carbon\Carbon;
 use App\Meedu\MeEdu;
 use App\Services\Member\Models\User;
 use App\Services\Order\Models\Order;
@@ -16,14 +17,65 @@ class DashboardController extends BaseController
 {
     public function index()
     {
+        // 今日注册用户数
         $todayRegisterUserCount = User::query()->where('created_at', '>=', date('Y-m-d'))->count();
-        $todayPaidNum = Order::todayPaidNum();
-        $todayPaidSum = Order::todayPaidSum();
+        // 总用户数
+        $userCount = User::query()->count();
+
+        // 昨日订单支付总额
+        $yesterdayPaidSum = (int)Order::query()
+            ->whereBetween('created_at', [Carbon::now()->subDays(1)->format('Y-m-d'), date('Y-m-d')])
+            ->where('status', Order::STATUS_PAID)
+            ->sum('charge');
+
+        // 今日订单支付总额
+        $todayPaidSum = (int)Order::query()
+            ->where('created_at', '>=', date('Y-m-d'))
+            ->where('status', Order::STATUS_PAID)
+            ->sum('charge');
+
+        // 进入付费用户数量
+        $todayPaidUserNum = Order::query()
+            ->select(['user_id'])
+            ->where('created_at', '>=', date('Y-m-d'))
+            ->where('status', Order::STATUS_PAID)
+            ->groupBy('user_id')
+            ->count();
+
+        // 昨日付费用户数量
+        $yesterdayPaidUserNum = Order::query()
+            ->select(['user_id'])
+            ->whereBetween('created_at', [Carbon::now()->subDays(1)->format('Y-m-d'), date('Y-m-d')])
+            ->where('status', Order::STATUS_PAID)
+            ->groupBy('user_id')
+            ->count();
+
+        // 本月收益
+        $thisMonthPaidSum = (int)Order::query()
+            ->where('created_at', '>=', date('Y-m') . '-01')
+            ->where('status', Order::STATUS_PAID)
+            ->sum('charge');
+        // 上个月收益
+        $lastMonthPaidSum = (int)Order::query()
+            ->whereBetween('created_at', [Carbon::now()->subMonths(1)->format('Y-m') . '-01', date('Y-m') . '-01'])
+            ->where('status', Order::STATUS_PAID)
+            ->sum('charge');
 
         return $this->successData([
             'today_register_user_count' => $todayRegisterUserCount,
-            'today_paid_num' => $todayPaidNum,
+            'user_count' => $userCount,
+
+            // 支付额度
             'today_paid_sum' => $todayPaidSum,
+            'yesterday_paid_sum' => $yesterdayPaidSum,
+
+            // 支付用户数
+            'today_paid_user_num' => $todayPaidUserNum,
+            'yesterday_paid_user_num' => $yesterdayPaidUserNum,
+
+            // 月度收益
+            'this_month_paid_sum' => $thisMonthPaidSum,
+            'last_month_paid_sum' => $lastMonthPaidSum,
         ]);
     }
 
