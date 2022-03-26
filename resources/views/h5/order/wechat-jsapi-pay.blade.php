148e7b226fbf3ba885b14fd13f17fb3a6fcb115e
none <none>
2022-03-26 20:58:28 +0800

    fixed: 微信JSAPI支付

--- resources/views/h5/order/wechat-jsapi-pay.blade.php
+++ resources/views/h5/order/wechat-jsapi-pay.blade.php
@@ -23,10 +23,10 @@
             function (res) {
                 if (res.err_msg === "get_brand_wcpay_request:ok") {
                     alert('支付成功');
-                    window.location = '{{request()->input('s_url')}}';
+                    window.location = decodeURIComponent('{{request()->input('s_url')}}');
                 } else {
                     alert('未支付');
-                    window.location = '{{request()->input('f_url')}}';
+                    window.location = decodeURIComponent('{{request()->input('f_url')}}');
                 }
             });
     }
