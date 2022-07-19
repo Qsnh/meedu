f11835148a1469125b90c5cb3341281602a6a08a
none <none>
2022-07-19 11:47:36 +0800

    移除无效的cookie操作代码

--- app/Listeners/UserLoginEvent/LoginRecordListener.php
+++ app/Listeners/UserLoginEvent/LoginRecordListener.php
@@ -10,8 +10,6 @@ namespace App\Listeners\UserLoginEvent;
 
 use Carbon\Carbon;
 use App\Events\UserLoginEvent;
-use App\Constant\FrontendConstant;
-use Illuminate\Support\Facades\Cookie;
 use App\Services\Member\Services\UserService;
 use App\Services\Member\Interfaces\UserServiceInterface;
 
@@ -44,11 +42,5 @@ class LoginRecordListener
         $at = $event->at ? Carbon::parse($event->at) : Carbon::now();
 
         $this->userService->createLoginRecord($event->userId, $event->platform, $ip, $at);
-
-        if ($event->platform === FrontendConstant::LOGIN_PLATFORM_H5 || $event->platform === FrontendConstant::LOGIN_PLATFORM_PC) {
-            // 将登录时间写入cookie
-            // 有效时间：默认15天
-            Cookie::queue(FrontendConstant::USER_LOGIN_AT_COOKIE_NAME, $at->timestamp, 3600 * 24 * 15);
-        }
     }
 }
