7f004bd1c78fbbccab631e2809f99fb3adffde3b
xxx <x@x.x>
2023-09-08 11:45:35 +0800

    删除无用代码

--- app/Constant/ConfigConstant.php
+++ app/Constant/ConfigConstant.php
@@ -12,14 +12,4 @@ class ConfigConstant
 {
 
     public const PRIVATE_MASK = '************';
-
-    public const WECHAT_PAY_CERT_CLIENT_KEY = 'pay.wechat.cert_client';
-    public const WECHAT_PAY_CERT_KEY_KEY = 'pay.wechat.cert_key';
-    public const WECHAT_PAY_CERT_CLIENT_PATH = 'private/wechat-pay-cert-client.pem';
-    public const WECHAT_PAY_CERT_KEY_KEY_PATH = 'private/wechat-pay-cert-key.pem';
-
-    public const ALIPAY_APP_CERT_PUBLIC_KEY_KEY = 'pay.alipay.app_cert_public_key';
-    public const ALIPAY_ROOT_CERT_KEY = 'pay.alipay.alipay_root_cert';
-    public const ALIPAY_APP_CERT_PUBLIC_KEY_PATH = 'private/alipay_app_cert_public_key.crt';
-    public const ALIPAY_ROOT_CERT_PATH = 'private/alipay_root_cert.crt';
 }
