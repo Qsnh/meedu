dcc3f5d2866a45579ad416c6cdd0cc2cd551e2b4
xxx <x@x.x>
2024-05-07 11:13:57 +0800

    优化启动脚本

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Waiting for mysql/redis to start..."
+
+sleep 15
+
 php /var/www/api/artisan migrate --force
 php /var/www/api/artisan install config
 php /var/www/api/artisan install role
